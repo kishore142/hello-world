ef594f6a2e82b3dd59af59840b1bbc52250565ab
kishore <rvsp_kishore@yahoo.com>
2015-10-19 17:05:30 +0530

    added new class

---
+++ HelloWorld/SecondViewControler.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewControler.h
+//  HelloWorld
+//
+//  Created by LST on 2015-10-19.
+//  Copyright (c) 2015 LST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SecondViewControler : NSObject
+
+@end
---
+++ HelloWorld/SecondViewControler.m
@@ -0,0 +1,13 @@
+//
+//  SecondViewControler.m
+//  HelloWorld
+//
+//  Created by LST on 2015-10-19.
+//  Copyright (c) 2015 LST. All rights reserved.
+//
+
+#import "SecondViewControler.h"
+
+@implementation SecondViewControler
+
+@end
