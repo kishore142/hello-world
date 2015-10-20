cb3ec0b6946f4802bbb14a3db34fbe34bea8bb00
kishore <rvsp_kishore@yahoo.com>
2015-10-20 17:08:35 +0530

    my mb 39 new changes

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSString *STR = @"hello";
+    
+    NSString *str1 = @"hello2";
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
